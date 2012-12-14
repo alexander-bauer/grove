86ab101f06b51c6f54493ffd4b3137867838b2e1
Sasha Crofter <sasha@crofter.org>
2012-12-14 00:37:48 -0500

    Add webui framework
    The ShowPath() function should return a complete HTML webpage created
    from the contents of a directory, the path of which is passed as the
    argument. If the directory does not contain a '.git' file, then it
    should be displayed as a directory listing. If it does, it should be
    treated as a repository directory, and information should be given as to
    how to clone it, as well as the log, and branches.

---
+++ webui.go
@@ -0,0 +1,6 @@
+package main
+
+//ShowPath takes a fully rooted path as an argument, and generates an HTML webpage in order in order to allow the user to navigate or clone via http.
+func ShowPath(path string) (page string) {
+	return "<html>Welcome to <a href=\"https://github.com/SashaCrofter/grove\">grove</a>.</html>"
+}
