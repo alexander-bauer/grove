7aaa3e4f967176cee575b6f581cb015e74bcb4d6
Sasha Crofter <sasha@crofter.org>
2012-12-13 22:23:58 -0500

    Add CGI framework and http server
    Grove, with this commit, will begin to have functionality. It can serve
    http, and invokes "git http-backend" using the CGI framework when the
    url ends in .git.

--- grove.go
+++ grove.go
@@ -1,18 +1,31 @@
 package main
 
 import (
-	//"net/http"
+	"io"
 	"log"
+	"net/http"
+	"net/http/cgi"
 	"os"
+	"strings"
 )
 
 const (
+	Version     = "0.0"
 	DefaultPort = 8860
 )
 
 const (
 	usage = "usage: %s [repositorydir] [logfile]\n"
 )
+
+var (
+	g *GitBackendHandler
+)
+
+type GitBackendHandler struct {
+	Handler *cgi.Handler
+	Logger  *log.Logger
+}
 
 func main() {
 	logger := log.New(os.Stdout, "", log.Ltime)
@@ -25,8 +38,41 @@ func main() {
 	Serve(logger, wd, DefaultPort)
 }
 
-func Serve(logger *log.Logger, repodir string, port int) {
-	logger.Println("Repository directory is:", repodir)
-	logger.Println("Binding to port:", port)
+func Serve(logger *log.Logger, repodir string, port int) (err error) {
+	g = &GitBackendHandler{
+		Handler: &cgi.Handler{
+			Path:   "git http-backend",
+			Root:   "/",
+			Dir:    repodir,
+			Env:    []string{"GIT_PROJECT_ROOT=" + repodir, "GIT_HTTP_EXPORT_ALL=TRUE"},
+			Logger: logger,
+		},
+		Logger: logger,
+	}
+	logger.Println("Created CGI handler:",
+		"\n\tPath:\t", g.Handler.Path,
+		"\n\tRoot:\t", g.Handler.Root,
+		"\n\tDir:\t", g.Handler.Dir,
+		"\n\tEnv:\t",
+		"\n\t\t", g.Handler.Env[0],
+		"\n\t\t", g.Handler.Env[1])
+
 	logger.Println("Starting server")
+	http.HandleFunc("/", HandleWeb)
+	err = http.ListenAndServe(":8080", nil)
+	return
+}
+
+func HandleWeb(w http.ResponseWriter, req *http.Request) {
+	//Send the request to the git http backend
+	//if it is to a .git URL.
+	if strings.HasSuffix(req.URL.String(), ".git") {
+		g.Logger.Println("Git request to", req.URL, "from", req.RemoteAddr)
+		g.Handler.ServeHTTP(w, req)
+		return
+	} else {
+		g.Logger.Println("View of", req.URL, "from", req.RemoteAddr)
+	}
+
+	io.WriteString(w, "<html>Welcome to <a href=\"https://github.com/SashaCrofter/grove\">grove</a>.")
 }
