94ed786febee5fe0902162ebd7a5d20b8404cbc8
Sasha Crofter <sasha@crofter.org>
2012-12-14 20:42:59 -0500

    Ensure removal of \r in system execution output
    This commit uses strings.Replace() to ensure that there are no \r
    characters in the output from execute().
    
    Windows environments remain untested, but this looks to be very stable,
    nonetheless.
    
    - close #2

--- git.go
+++ git.go
@@ -13,20 +13,21 @@ func setExecPath() (err error) {
 	//Use 'git --exec-path' to get the path
 	//of the git executables.
 	path, err := execute("", "git", "--exec-path")
-	execPath = strings.TrimRight(path, "\r\n")
+	execPath = strings.TrimRight(path, "\n")
 	return
 }
 
 func gitBranch(path string) (branch string) {
 	branch, _ = execute(path, "git", "rev-parse", "--abbrev-ref", "HEAD")
-	return strings.TrimRight(branch, "\r\n")
+	return strings.TrimRight(branch, "\n")
 }
 
+//Execute invokes exec.Command() with the given command, arguments, and working directory. All CR ('\r') characters are removed in output.
 func execute(dir, command string, args ...string) (output string, err error) {
 	cmd := exec.Command(command, args...)
 	if len(dir) != 0 {
 		cmd.Dir = dir
 	}
 	out, err := cmd.Output()
-	return string(out), err
+	return strings.Replace(string(out), "\r", "", 0), err
 }
