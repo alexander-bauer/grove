78e3078d442c40748ec188c0fa1125a841eb0d72
Sasha Crofter <sasha@crofter.org>
2013-03-24 14:16:18 -0400

    Add XML API and use Accept in the http header
    This commit adds XML support to the API. This can be requested with
    `?api=xml` or by specifying `Accept: application/xml` (or anything
    ending with `/xml` in the http header.
    
    The http form (`?api`) is analyzed *before* the header, though, so it
    acts as an override.
    
    As it stands, the form must be submitted and fail before it falls back
    to encoding type.
    
    Close #91

--- api.go
+++ api.go
@@ -4,8 +4,10 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"errors"
 	"net/http"
+	"strings"
 )
 
 // encoder is a private interface which all encoding/*.Encoder types
@@ -27,17 +29,45 @@ var (
 
 func ServeAPI(w http.ResponseWriter, req *http.Request, g *git, ref string, maxCommits int) (err error) {
 	// First, determine the encoding and error if it isn't appropriate
-	// or supported.
+	// or supported. To do this, we need to check the api value and
+	// Accept header. We also want to include the Content-Type.
 	var e encoder
+	var c string // The Content-Type field in the http.Response
 	switch req.FormValue("api") {
 	case "json":
 		// The json.Encoder type implements our private encoder
 		// interface, because it has the function Encode().
+		c = "application/json"
 		e = json.NewEncoder(w)
+	case "xml":
+		// Same as above.
+		c = "application/xml"
+		e = xml.NewEncoder(w)
 	}
+	// If the api field wasn't submitted in the form, we should still
+	// check the Accept header.
+	accept := strings.Split(strings.Split(
+		req.Header.Get("Accept"), ";")[0],
+		",")
+	if e == nil && len(accept) != 0 {
+		// Now we must loop through each element in accept, because
+		// there can be multiple values to the Accept key. As soon as
+		// we find an acceptable encoding, break the loop.
+		for _, a := range accept {
+			switch {
+			case strings.HasSuffix(a, "/json"):
+				e = json.NewEncoder(w)
+			case strings.HasSuffix(a, "/xml"):
+				e = xml.NewEncoder(w)
+			}
+			if e != nil {
+				c = a // Set the content type appropriately.
+				break
+			}
+		}
+	}
+	// If the encoding is invalid or not provided, return this error.
 	if e == nil {
-		// If the encoding is invalid or not provided, return this
-		// error.
 		return InvalidEncodingError
 	}
 	// If an encoding was provided, prepare a response.
@@ -46,6 +76,8 @@ func ServeAPI(w http.ResponseWriter, req *http.Request, g *git, ref string, maxC
 		Ref:        g.SHA("HEAD"),
 		Commits:    g.Commits(ref, maxCommits),
 	}
+	// Set the Content-Type appropriately in the header.
+	w.Header().Set("Content-Type", c)
 
 	// Finally, encode to the http.ResponseWriter with whatever
 	// encoder was selected.
